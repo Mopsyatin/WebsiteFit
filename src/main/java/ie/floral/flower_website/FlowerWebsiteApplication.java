b9ccac2c2ade346349d9dc60fbe6b181c5ad3f4d
Mikhael <131536729+Mopsyatin@users.noreply.github.com>
2026-01-22 05:38:09 +0000

    -updated email send form

--- src/main/java/ie/floral/flower_website/FlowerWebsiteApplication.java
+++ src/main/java/ie/floral/flower_website/FlowerWebsiteApplication.java
@@ -3,8 +3,10 @@ package ie.floral.flower_website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class FlowerWebsiteApplication {
 
 
--- src/main/java/ie/floral/flower_website/service/EmailSendService.java
+++ src/main/java/ie/floral/flower_website/service/EmailSendService.java
@@ -1,7 +1,9 @@
 package ie.floral.flower_website.service;
 
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Async;
 
+@Async
 public interface EmailSendService {
     void sendContactEmail(String email, String subject, String body);
 }
